7f69f7cbcc1b149095d8a7c89aba71315f788492
Roma595 <kotkov.roma04@mail.ru>
2025-10-22 10:30:55 +0300

    delete mvcmovie

--- MvcMovieKotkov/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MvcMovieKotkov.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
